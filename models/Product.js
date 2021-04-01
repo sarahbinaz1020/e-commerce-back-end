e2ddae7cd6e59c2a34cb867ce8526a993213acb1
sarahbinaz1020 <sarahbinaz1020@gmail.com>
2021-03-31 20:15:26 -0700

    define columns in models

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,18 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    product_name: {
+      type: DataTypes.STRING
+    },
+    price: {
+      type: DataTypes.INTEGER
+    },
+    stock: {
+      type: DataTypes.INTEGER
+    },
+    category_id: {
+      type: DataTypes.INTEGER
+    },
   },
   {
     sequelize,
